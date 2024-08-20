0587159a75a6ca4ed37825cf40c567d1e966c8cb
Bizoux <andriatsitohainarivo7@gmail.com>
2024-08-20 20:00:49 +0200

    maj 3 composant mpiara-manompo

--- src/components/MpiaraManompo.vue
+++ src/components/MpiaraManompo.vue
@@ -291,7 +291,7 @@ export default{
   background-size: 300% 300%;
   animation: color 12s ease-in-out infinite;
   }
-
+/* 
   @keyframes color{
   0%{
     background-position: 0 50%;
@@ -302,7 +302,7 @@ export default{
   100%{
     background-position: 0 50%;
   }
-}
+} */
 
   /* Menu */
   .navbar a{
